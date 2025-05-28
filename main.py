9471ebf65c674d5d4f0b433b3db0005590b0f0b5
Mironmanom <mironlearn@gmail.com>
2025-05-28 21:41:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import telebot
+
+   
+
+bot = telebot.TeleBot('Token')
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+
+
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+@bot.message_handler(commands=['Meow'])
+def send_meow(message):
+    bot.reply_to(message,'мяу')
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message,'Список команд:/help - список всех комманд,/heh - пишет he указанное количество раз,/start,/hello - приветствие')
+bot.polling()
